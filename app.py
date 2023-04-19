1e0d2a0fa64fb75d49e5e66f4e934b22c42d10a8
JaykumarPatel4802 <jay.patel4802@gmail.com>
2023-04-19 17:57:13 -0500

    start ui

--- app.py
+++ app.py
@@ -5,15 +5,21 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import nltk
 import math
 import torch
+import lyricsgenius
+from unidecode import unidecode
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
 
 model_name = "afnanmmir/t5-base-abstract-to-plain-language-1"
 max_input_length = 1024
 max_output_length = 256
 min_output_length = 64
 
-st.header("Generate summaries for articles")
+st.header("Get your song recommendations here!")
 
-st_model_load = st.text('Loading summary generator model...')
+st_model_load = st.text('Loading song generator model...')
 
 @st.cache(allow_output_mutation=True)
 def load_model():
@@ -30,24 +36,57 @@ st_model_load.text("")
 
 if 'text' not in st.session_state:
     st.session_state.text = ""
-st_text_area = st.text_area('Text to generate the summary for', value=st.session_state.text, height=500)
+st_text_area = st.text_area('Song to generate recommendations for', value=st.session_state.text, height=500)
 
-def generate_summary():
+
+genius_key = os.environ.get('LYRIC_GENIUS_KEY')
+genius = lyricsgenius.Genius(genius_key, skip_non_songs=True, excluded_terms=["(Remix)", "(Live)"], remove_section_headers=True, timeout = 60)
+
+def get_lyrics(song_name, artist_name):
+    try:
+        song = genius.search_song(song_name, artist_name)
+    except:
+        return "ERROR"
+    original_lyrics = song.lyrics
+    lyrics = original_lyrics.split('\n')[1:]
+    newLyrics = list()
+    for i in lyrics:
+        if i != "" and i[0] != "[" and i[len(i) - 1] != "]":
+            newLyrics.append(i)
+    newLyrics = " ".join(newLyrics)
+    clean_lyrics = unidecode(newLyrics)
+    if len(clean_lyrics) > 1024:
+        clean_lyrics = clean_lyrics[:1024]
+    return clean_lyrics
+
+def generate_song():
     st.session_state.text = st_text_area
 
+    input_text = st_text_area
+    input_text = input_text.split(' by ')
+    song_name = input_text[0]
+    artist_name = input_text[1]
+    lyrics = get_lyrics(song_name, artist_name)
+    print("Lyrics:")
+    print(lyrics)
+
     # tokenize text
-    inputs = ["summarize: " + st_text_area]
+    inputs = ["summarize: " + lyrics]
     inputs = tokenizer(inputs, return_tensors="pt", max_length=max_input_length, truncation=True)
 
     # compute predictions
     outputs = model.generate(**inputs, do_sample=True, max_length=max_output_length, early_stopping=True, num_beams=8, length_penalty=2.0, no_repeat_ngram_size=2, min_length=min_output_length)
     decoded_outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
     predicted_summaries = nltk.sent_tokenize(decoded_outputs.strip())
+    print("Predicted Summaries: ")
+    print(predicted_summaries)
+    print(type(predicted_summaries))
+    overall_summary =  " ".join(predicted_summaries)
 
-    st.session_state.summaries = predicted_summaries
+    st.session_state.summaries = [overall_summary]
 
 # generate summary button
-st_generate_button = st.button('Generate summary', on_click=generate_summary)
+st_generate_button = st.button('Generate summary', on_click=generate_song)
 
 # summary generation labels
 if 'summaries' not in st.session_state:
